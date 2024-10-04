474d6eaf9d67457a2f60ba41f50442fd224a0b3d
nemethdavid03 <nmthdvd123@gmail.com>
2024-10-04 19:35:05 +0200

    Updated GeminiCard.tsx

--- src/app/admin/ai/_components/GeminiCard.tsx
+++ src/app/admin/ai/_components/GeminiCard.tsx
@@ -24,19 +24,19 @@ const GeminiCard = () => {
                 </Link>
             </CardFooter>
         </Card>
-        <Card className="col-span-12 md:col-span-6 row-start-2 md:row-start-1 col-start-7 md:col-start-1 bg-gradient-to-r from-cyan-400 to-cyan-600 text-black transition-all duration-300">
+        <Card className="col-span-12 md:col-span-6 row-start-2 md:row-start-1 col-start-1 md:col-start-1 bg-gradient-to-r from-blue-400 to-blue-600 text-black transition-all duration-300">
             <CardHeader className="flex flex-col">
                 <div className="flex items-center space-x-2">
 
-                    <CardTitle className="text-lg font-bold">ChatGPT</CardTitle>
+                    <CardTitle className="text-lg font-bold">Bard AI</CardTitle>
                 </div>
                 <CardDescription className="mt-2 text-sm text-gray-900">
-                    Használj egy fejlett nyelvi modellt a szöveg generálásához, a fordításhoz és még sok máshoz.
+                    A Bard egy nagyméretű nyelvi modell, amelyet a Google fejlesztett ki.
                 </CardDescription>
             </CardHeader>
 
             <CardFooter className="flex justify-between items-center mt-4 dark:text-white">
-                <Link href="ai/chatgpt" passHref>
+                <Link href="ai/bard" passHref>
                     <Button variant="outline">Konfigurálás <Settings2 size={13} className="ms-3" /></Button>
                 </Link>
             </CardFooter>
